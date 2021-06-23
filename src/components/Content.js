4d8e55bebfa7a0493cce4d014a8c2e15c4652313
dannys1 <dani_stevenson123@yahoo.co.id>
2021-06-23 16:02:19 +0700

    Change Text Content

--- src/components/Content.js
+++ src/components/Content.js
@@ -9,7 +9,7 @@ function Content() {
             <div className="menu-card">
                 <img src={ImageOne} alt="egg" className="h-full rounded mb-20 shadow" />
                 <div className="center-content">
-                    <h2 className="text-2xl mb-2">Egg Muffins</h2>
+                    <h2 className="text-2xl mb-2">Biji Andre</h2>
                     <p className="mb-2">Crispy, delicious, and nutrious</p>
                     <span>$16</span>
                 </div>
@@ -17,7 +17,7 @@ function Content() {
             <div className="menu-card">
                 <img src={ImageTwo} alt="egg" className="h-full rounded mb-20 shadow" />
                 <div className="center-content">
-                    <h2 className="text-2xl mb-2">Egg Salad</h2>
+                    <h2 className="text-2xl mb-2">Salad Andre</h2>
                     <p className="mb-2">Crispy, delicious, and nutrious</p>
                     <span>$18</span>
                 </div>
--- src/pages/about.js
+++ src/pages/about.js
@@ -4,8 +4,9 @@ function About() {
     return (
         <div className="h-screen flex justify-center items-center bg-yellow-300">
             <h1 className="text-9xl uppercase font-black">
-                About Page
+                About Andre
             </h1>
+            
         </div>
     )
 }
