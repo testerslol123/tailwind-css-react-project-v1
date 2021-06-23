180c591debe171b524d5150ab78f9a2f235bdab8
dannys1 <dani_stevenson123@yahoo.co.id>
2021-06-23 14:04:34 +0700

    Complete the tutorial of youtube

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Content from './components/Content';
 import Footer from './components/Footer';
+import Home from './pages';
+import About from './pages/about';
+import Menu from './pages/menu';
+import {Switch, Route} from 'react-router-dom';
+import Dropdown from './components/Dropdown';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  }
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+      }
+    }
+
+    window.addEventListener('resize', hideMenu);
+
+    return () => {
+      window.removeEventListener('resize', hideMenu);
+    }
+
+  }, [])
+
+
   return (
     <>
-      <Navbar />
-      <Hero />
-      <Content />
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/about" exact component={About} />
+      <Route path="/menu" exact component={Menu} />
+      </Switch>
       <Footer />
     </>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <div className="flex justify-center items-center h-16 items-center bg-black text-white">
-
+            <p>Copyright 2021 EGG All rights reserved</p>
         </div>
     )
 }
